d3314e92e77fdc6aae226dcc95fffe4125ac7a23
Thomas PHIMANESONE <20150195@B15-06.local>
2019-07-01 12:08:48 +0200

    API response

--- screens/CameraScreen/index.js
+++ screens/CameraScreen/index.js
@@ -1,60 +1,78 @@
-import React, { Component } from "react";
-import { Text, View, TouchableOpacity } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
-import { Camera } from 'expo-camera';
 
-export default class CameraScreen extends Component {
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default class CameraScreen extends React.Component {
   state = {
     hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
+    scanned: false,
   };
 
   static navigationOptions = {
-    title: "CameraScreen",
-    header:null
-  };
+        title: "CameraScreen",
+        header : null
+      };
 
   async componentDidMount() {
+    this.getPermissionsAsync();
+  }
+
+  getPermissionsAsync = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
+  };
+
+    async getMoviesFromApiAsync() {
+    try {
+            const response = await fetch('https://fr.openfoodfacts.org/api/v0/produit/8024884500403.json');
+            const responseJson = await response.json();
+            console.log(responseJson);
+            return responseJson.movies;
+        }
+        catch (error) {
+            console.error(error);
+        }
   }
 
   render() {
-    const { hasCameraPermission } = this.state;
+    const { hasCameraPermission, scanned } = this.state;
+
     if (hasCameraPermission === null) {
-      return <View />;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      );
+      return <Text>Requesting for camera permission</Text>;
     }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+        }}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+
+        {scanned && (
+          <Button title={'Tap to Scan Again'} onPress={() => this.setState({ scanned: false })} />
+        )}
+      </View>
+    );
   }
+
+  handleBarCodeScanned = ({ type, data }) => {
+    this.setState({ scanned: true });
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    this.getMoviesFromApiAsync();
+  };
 }
+
+
+//data.product.generic_name_fr
+//data.product.code.keywords.
+//data.product.packaging_tags
