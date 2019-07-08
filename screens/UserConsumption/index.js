f351dcdcae8e5bbb4e92bffcfe42f47506a90376
Benjamin Benoit <benjaminbenoitfr@gmail.com>
2019-07-08 08:58:11 +0200

    consumptions with empty listview

--- screens/UserConsumption/index.js
+++ screens/UserConsumption/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Environment from "./../../Environment";
 
 export default class FlatListBasics extends Component {
 
@@ -15,24 +16,112 @@ export default class FlatListBasics extends Component {
         // headerLeft: null
       };
 
+
+
+      constructor(props){
+        super(props);
+        this.state = {
+          locationResult: { latitude: 0, longitude: 0 },
+          errorMessage: null,
+          hasLocationPermissions: false,
+          region: null,
+          location: null,
+          userLocation: {
+            latitude: null,
+            longitude: null,
+            latitudeDelta: null,
+            longitudeDelta: null
+          },
+          user:{},
+          consumes:[],
+          dataSource: null
+        }
+      }
+
+  // _fetchData(callback) {
+  //   const params = this.state._dataAfter !== ''
+  //     ? `&after=${this.state._dataAfter}`
+  //     : '';
+  //   //Limits fetches to 15 so there's lesser items from the get go
+  //   fetch(Environment.CLIENT_API + "/api/consume/getByUserId/" + user.id)
+  //     .then(response => response.json())
+  //     .then(callback)
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  //   }
+
+
+
+  componentDidMount(){
+    this._getConsumptions();
+    console.log(this.state.consumes)
+  }
+
+  _getConsumptions = async () => {
+    const response = await fetch(Environment.CLIENT_API + "/api/consume/getByUserId/" + this.state.user.id, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "GET"
+    });
+
+    const json = await response.json();
+    this.setState({ consumes: json.consumes });
+  };
+
     render() {
-      return (
-        <View style={styles.container}>
+      // if (this.state.isLoading) {
+      //   return (
+      //     <View style={styles.container}>
+      //       <ActivityIndicator size="large" />
+      //     </View>
+      //   );
+      // } else {
+        return (
           <FlatList
-            data={[
-              {key: 'Devin'},
-              {key: 'Jackson'},
-              {key: 'James'},
-              {key: 'Joel'},
-              {key: 'John'},
-              {key: 'Jillian'},
-              {key: 'Jimmy'},
-              {key: 'Julie'},
-            ]}
-            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+          data={this.state.consumes}
+            renderRow={rowData => {
+              return (
+                <View style={styles.listItem}>
+                  {/* <View style={styles.imageWrapper}>
+                    <Image
+                      style={{ width: 70, height: 70 }}
+                      source={{
+                        uri: rowData.data.icon_img === '' ||
+                          rowData.data.icon_img === null
+                          ? 'https://via.placeholder.com/70x70.jpg'
+                          : rowData.data.icon_img,
+                      }}
+                    />
+                  </View> */}
+                  <View style={{ flex: 1 }}>
+                    <Text style={styles.title}>
+                      {rowData.data.display_name}
+                    </Text>
+                    {/* <TouchableOpacity
+                      onPress={() => Linking.openURL('https://www.google.com')}>
+                      <Text style={styles.subtitle}>
+                        {rowData.data.public_description}
+                      </Text>
+                    </TouchableOpacity> */}
+                  </View>
+                </View>
+              );
+            }}
+            // onEndReached={() =>
+            //   this.setState({ isLoadingMore: true }, () => this.fetchMore())}
+            // renderFooter={() => {
+            //   return (
+            //     this.state.isLoadingMore &&
+            //     <View style={{ flex: 1, padding: 10 }}>
+            //       <ActivityIndicator size="small" />
+            //     </View>
+            //   );
+            // }}
           />
-        </View>
-      );
+        );
+      // }
     }
   }
   
